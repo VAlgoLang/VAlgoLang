6c1e80684acfe6106228cd12f2f7acb3b6830030
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-10-11 20:11:40 +0100

    Added exit codes and implementation of error handling for syntax errors

--- src/main/kotlin/com/manimdsl/ManimDSLParser.kt
+++ src/main/kotlin/com/manimdsl/ManimDSLParser.kt
@@ -2,25 +2,47 @@ package com.manimdsl
 
 import antlr.ManimLexer
 import antlr.ManimParser
+import com.manimdsl.errorHandling.ErrorHandler
+import com.manimdsl.errorHandling.SyntaxErrorListener
+import com.manimdsl.errorHandling.SyntaxErrorStrategy
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
 import java.io.InputStream
+import kotlin.system.exitProcess
+
+/* Exit status codes */
+enum class ExitStatus(val code: Int) {
+    EXIT_SUCCESS(0),
+    SYNTAX_ERROR(100),
+    SEMANTIC_ERROR(200),
+    PATH_ERROR(101)
+}
 
 class ManimDSLParser(private val input: InputStream) {
 
     // Build ANTLR Parse Tree and if Syntax Errors found, throw them and exit
     fun parseFile(): ManimParser.ProgramContext {
         val input = CharStreams.fromStream(input)
+        // Lexical analysis
         val lexer = ManimLexer(input)
         lexer.removeErrorListeners()
+
+        // Syntax analysis
         val tokens = CommonTokenStream(lexer)
         val parser = ManimParser(tokens)
+
+        parser.errorHandler = SyntaxErrorStrategy()
         parser.removeErrorListeners()
-//        parser.addErrorListener(SyntaxErrorListener())
-        val program = parser.program()
-//        if (parser.numberOfSyntaxErrors > 0) {
-//            exitProcess(SYNTAX_ERROR)
-//        }
+        parser.addErrorListener(SyntaxErrorListener())
+
+        val program = parser.program();
+
+        // Error handling
+        val exitStatus = ErrorHandler.checkErrors()
+        if (exitStatus != ExitStatus.EXIT_SUCCESS) {
+            exitProcess(exitStatus.code)
+        }
+
         return program
     }
 
