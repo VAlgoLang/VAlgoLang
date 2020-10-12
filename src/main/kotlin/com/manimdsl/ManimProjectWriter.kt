6155a31ab07f3f2f0e77a4c00e83ecf5763f6e8e
Mayank Sharma <mayank.sharma.uk@gmail.com>
2020-10-12 20:35:35 +0100

    Refactored file generation for manim project file

--- src/main/kotlin/com/manimdsl/ManimProjectWriter.kt
+++ src/main/kotlin/com/manimdsl/ManimProjectWriter.kt
@@ -4,18 +4,19 @@ import java.io.File
 
 class ManimProjectWriter(private val pythonCode: String) {
 
-    fun createPythonFile(createTempFile: Boolean, fileName: String): String {
-        return if(createTempFile) {
-            val tempFile = createTempFile(pythonCode)
-            tempFile.writeText(pythonCode)
-            tempFile.absolutePath
-        } else {
+    fun createPythonFile(fileName: String? = null): String {
+        return if (fileName !== null) {
             File(fileName).writeText(pythonCode)
             fileName
+        } else {
+            val tempFile = createTempFile(suffix = ".py")
+            tempFile.writeText(pythonCode)
+            tempFile.path
         }
     }
 
     fun generateAnimation(fileName: String): Int {
+        println(fileName)
         val process = ProcessBuilder("manim $fileName Main -pl".split(" ")).start()
 
         return process.waitFor()
--- src/test/kotlin/com/manimdsl/linearrepresentation/TestLinearRepresentation.kt
+++ src/test/kotlin/com/manimdsl/linearrepresentation/TestLinearRepresentation.kt
@@ -8,7 +8,7 @@ class TestLinearRepresentation {
     @Test
     fun mockStackLinearRepresntation() {
 
-        val codeBlock = listOf("let y = new Stack;", "y.push(2);", "y.pop();");
+        val codeBlock = listOf("let y = new Stack;", "y.push(2);", "y.push(3);", "y.pop();");
 
         val stackIR = listOf(
             CodeBlock(codeBlock, "code_block", "code_text", "pointer"),
@@ -17,13 +17,16 @@ class TestLinearRepresentation {
             MoveToLine(2, "pointer", "code_block"),
             MoveObject("testIdent", "y", ObjectSide.ABOVE),
             MoveToLine(3, "pointer", "code_block"),
-            MoveObject("testIdent", "y", ObjectSide.ABOVE, 5),
+            NewObject(Rectangle("3"), "testIdent1"),
+            MoveObject("testIdent1", "testIdent", ObjectSide.ABOVE),
+            Sleep(2.0),
+            MoveToLine(4, "pointer", "code_block"),
+            MoveObject("testIdent1", "testIdent", ObjectSide.ABOVE, 20),
             Sleep(2.0)
         )
 
         val writer = ManimProjectWriter(ManimWriter(stackIR).build())
-        writer.createPythonFile(false, "test.py")
-        writer.generateAnimation("test.py")
+        writer.generateAnimation(writer.createPythonFile())
 
 //        println(ManimWriter(stackIR).build())
 //        assertEquals(ManimDSLParser(inputFile.inputStream()).parseFile().first, ExitStatus.EXIT_SUCCESS)
