226127e6d13574c1c98a7cb56c6e9f75ed18e3df
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-10-12 12:04:13 +0100

    Implemented more detailed tests based on specific errors

--- src/test/kotlin/com/manimdsl/syntaxAnalysis/InvalidSyntaxTests.kt
+++ src/test/kotlin/com/manimdsl/syntaxAnalysis/InvalidSyntaxTests.kt
@@ -3,17 +3,24 @@ package com.manimdsl.syntaxAnalysis
 import com.manimdsl.ExitStatus
 import com.manimdsl.ManimDSLParser
 import org.junit.Assert.assertEquals
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
+import org.junit.Assert.assertTrue
+import org.junit.jupiter.api.*
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.io.ByteArrayOutputStream
 import java.io.File
+import java.io.PrintStream
 import java.util.stream.Stream
 import kotlin.streams.asStream
 
+
 class InvalidSyntaxTests {
+    private val standardOut = System.out
+    private val outputStreamCaptor = ByteArrayOutputStream()
+
     companion object {
+
         @JvmStatic
         fun data(): Stream<Arguments> {
             return File("src/test/testFiles/invalid/syntaxErrors").walk().filter { it.isFile }
@@ -26,11 +33,24 @@ class InvalidSyntaxTests {
             File("src/test/testFiles/bin").mkdir()
         }
 
-        @AfterAll
+
         @JvmStatic
+        @AfterAll
         internal fun afterAll() {
             File("src/test/testFiles/bin").deleteRecursively()
         }
+    }
+
+    // To capture standard out e.g. use of println
+    @BeforeEach
+    internal fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    // Reset system out to stdout
+    @AfterEach
+    internal fun tearDown() {
+        System.setOut(standardOut)
     }
 
     @ParameterizedTest(name = "{0}")
@@ -38,6 +58,22 @@ class InvalidSyntaxTests {
     fun invalidSyntaxErrorTests(fileName: String) {
         val inputFile = File(fileName)
         assertEquals(ExitStatus.SYNTAX_ERROR, ManimDSLParser(inputFile.inputStream()).parseFile().first)
+    }
+
+    @Test
+    fun mismatchedInput() {
+        val inputFile = File("src/test/testFiles/invalid/syntaxErrors/mismatchedInput.manimdsl")
+        ManimDSLParser(inputFile.inputStream()).parseFile()
+        assertTrue(
+            outputStreamCaptor.toString().contains(Regex("Syntax error at \\d*:\\d*: mismatched input .* expecting .*"))
+        )
+    }
+
+    @Test
+    fun missingToken() {
+        val inputFile = File("src/test/testFiles/invalid/syntaxErrors/missingToken.manimdsl")
+        ManimDSLParser(inputFile.inputStream()).parseFile()
+        assertTrue(outputStreamCaptor.toString().contains(Regex("Syntax error at \\d*:\\d*: missing .* at .*")))
     }
 
 }
