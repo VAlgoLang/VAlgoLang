84a326b4cd0f299b26a45ca70279cc319d89cb37
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-10-13 22:58:35 +0100

    Added animationMethods for stack class. And new variable state node

--- src/main/kotlin/com/manimdsl/frontend/AbstractSyntaxTree.kt
+++ src/main/kotlin/com/manimdsl/frontend/AbstractSyntaxTree.kt
@@ -34,9 +34,109 @@ data class SubtractExpression(override val lineNumber: Int, override val expr1: 
 data class MultiplyExpression(override val lineNumber: Int, override val expr1: ExpressionNode, override val expr2: ExpressionNode): BinaryExpression(lineNumber, expr1, expr2)
 
 // Unary Expressions
-sealed class UnaryExpression(override val lineNumber: Int, open val expr: ExpressionNode): ExpressionNode(lineNumber)
-data class PlusExpression(override val lineNumber: Int, override val expr: ExpressionNode): UnaryExpression(lineNumber, expr)
-data class MinusExpression(override val lineNumber: Int, override val expr: ExpressionNode): UnaryExpression(lineNumber, expr)
+sealed class UnaryExpression(override val lineNumber: Int, open val expr: ExpressionNode) : ExpressionNode(lineNumber)
+data class PlusExpression(override val lineNumber: Int, override val expr: ExpressionNode) :
+    UnaryExpression(lineNumber, expr)
 
+data class MinusExpression(override val lineNumber: Int, override val expr: ExpressionNode) :
+    UnaryExpression(lineNumber, expr)
+
+
+// Types (to be used in symbol table also)
+sealed class Type : ASTNode()
+
+// Primitive / Data structure distinction requested by code generation
+sealed class PrimitiveType : Type()
+object NumberType : PrimitiveType()
+
+sealed class DataStructureType(
+    open var internalType: Type,
+    open val methods: HashMap<String, DataStructureMethod>
+) : Type() {
+    abstract fun containsMethod(method: String): Boolean
+    abstract fun getMethodByName(method: String): DataStructureMethod
+
+    /** Init methods are to return instructions needed to create MObject **/
+    abstract fun init(
+        identifier: String,
+        x: Int,
+        y: Int,
+        variableName: String = "empty"
+    ): Pair<List<ManimInstr>, Object>
+}
+
+abstract class DataStructureMethod(open val returnType: Type, open var argumentTypes: List<Type>) {
+    abstract fun animateMethod(
+        arguments: List<String>,
+        options: Map<String, Any> = emptyMap()
+    ): Pair<List<ManimInstr>, Object?>
+}
+
+data class StackType(
+    override var internalType: Type = NumberType,
+    override val methods: HashMap<String, DataStructureMethod> = hashMapOf(
+        "push" to PushMethod(
+            argumentTypes = listOf(
+                NumberType
+            )
+        ), "pop" to PopMethod(internalType)
+    )
+) : DataStructureType(internalType, methods) {
+
+    data class PushMethod(override val returnType: Type = NoType, override var argumentTypes: List<Type>) :
+        DataStructureMethod(returnType, argumentTypes) {
+        /** arguments = {value} **/
+        override fun animateMethod(
+            arguments: List<String>,
+            options: Map<String, Any>
+        ): Pair<List<ManimInstr>, Object?> {
+            val rectangle = NewObject(Rectangle(arguments[0]))
+            return Pair(
+                listOf(
+                    rectangle,
+                    MoveObject(rectangle.ident, (options["top"] as Object).ident, ObjectSide.ABOVE),
+                ), rectangle
+            )
+        }
+
+    }
+
+    data class PopMethod(override val returnType: Type, override var argumentTypes: List<Type> = listOf()) :
+        DataStructureMethod(returnType, argumentTypes) {
+        override fun animateMethod(
+            arguments: List<String>,
+            options: Map<String, Any>
+        ): Pair<List<ManimInstr>, Object?> {
+            return Pair(
+                listOf(
+                    MoveObject(
+                        (options["top"] as Object).ident,
+                        (options["second"] as Object).ident,
+                        ObjectSide.ABOVE,
+                        20,
+                        options["fadeOut"] as Boolean
+                    ),
+                ), null
+            )
+        }
+    }
+
+    override fun containsMethod(method: String): Boolean {
+        return methods.containsKey(method)
+    }
+
+    override fun getMethodByName(method: String): DataStructureMethod {
+        return methods[method]!!
+    }
+
+    override fun init(identifier: String, x: Int, y: Int, variableName: String): Pair<List<ManimInstr>, Object> {
+        val stackInit = InitStructure(x, y, Alignment.HORIZONTAL, identifier, variableName)
+
+        // Add to stack of objects to keep track of identifier
+        return Pair(listOf(stackInit), stackInit)
+    }
+}
+
+object NoType: Type()
 // This is used to collect arguments up into method call node
 data class ArgumentNode(val arguments: List<ExpressionNode>) : ASTNode()
