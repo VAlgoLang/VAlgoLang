daa4287cae0e2eaa34d311404a5de517509e20d5
Rini Banerjee <rbanerjee20@gmail.com>
2020-10-13 09:58:02 +0100

    Cleanup in DataStructureHandler.

--- src/main/kotlin/com/manimdsl/frontend/DataStructureHandler.kt
+++ src/main/kotlin/com/manimdsl/frontend/DataStructureHandler.kt
@@ -1,10 +1,6 @@
 package com.manimdsl.frontend
 
 class DataStructureHandler {
-    enum class DataStructure {
-        STACK,
-        NONE
-    }
 
     private fun convertTypeToDataStructure(type: DataStructureType): DataStructure {
         return when (type) {
@@ -23,31 +19,34 @@ class DataStructureHandler {
         }
     }
 
-
-
 }
 
-sealed class DataStructureMethod(open val numberOfArguments: Int, open val dataStructure: DataStructureHandler.DataStructure) {
+enum class DataStructure {
+    STACK,
+    NONE
+}
+
+
+sealed class DataStructureMethod(open val numberOfArguments: Int, open val dataStructure: DataStructure) {
     open fun hasValidNumberOfArguments(numArgs: Int): Boolean {
         return numberOfArguments == numArgs
     }
-
 }
 
 
-object StackPop : DataStructureMethod(numberOfArguments = 0, dataStructure = DataStructureHandler.DataStructure.STACK) {
+object StackPop : DataStructureMethod(numberOfArguments = 0, dataStructure = DataStructure.STACK) {
     override fun toString(): String {
         return "pop"
     }
 }
 
-object StackPush : DataStructureMethod(numberOfArguments = 1, dataStructure = DataStructureHandler.DataStructure.STACK) {
+object StackPush : DataStructureMethod(numberOfArguments = 1, dataStructure = DataStructure.STACK) {
     override fun toString(): String {
         return "push"
     }
 }
 
-object ErrorMethod : DataStructureMethod(numberOfArguments = -1, dataStructure = DataStructureHandler.DataStructure.NONE) {
+object ErrorMethod : DataStructureMethod(numberOfArguments = -1, dataStructure = DataStructure.STACK) {
     override fun hasValidNumberOfArguments(numArgs: Int): Boolean = false
 }
 
