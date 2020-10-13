850b9738be3df0474e3da7832f12dc06b4ef5881
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-10-13 23:36:58 +0100

    Created variable name generator.

--- src/main/kotlin/com/manimdsl/shapes/Shapes.kt
+++ src/main/kotlin/com/manimdsl/shapes/Shapes.kt
@@ -1,21 +1,15 @@
 package com.manimdsl.shapes
 
-import java.util.*
-
 interface Shape {
 
     val text: String
     val classPath: String
     val className: String
-
-    fun generateVariableName(): String
+    val pythonVariablePrefix: String
 }
 
 data class Rectangle(override val text: String) : Shape {
     override val classPath: String = "pythonLib/rectangle.py"
     override val className: String = "Rectangle_block"
-
-    override fun generateVariableName(): String {
-        return UUID.randomUUID().toString()
-    }
+    override val pythonVariablePrefix: String = "rectangle"
 }
