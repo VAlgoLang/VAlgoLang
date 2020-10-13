756182324adf008167e0c09386e8833e254d8aa7
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-10-13 11:38:47 +0100

    Implemented code generation test

--- src/test/kotlin/com/manimdsl/linearrepresentation/TestLinearRepresentation.kt
+++ src/test/kotlin/com/manimdsl/linearrepresentation/TestLinearRepresentation.kt
@@ -3,12 +3,19 @@ package com.manimdsl.linearrepresentation
 import com.manimdsl.ManimProjectWriter
 import com.manimdsl.ManimWriter
 import com.manimdsl.shapes.Rectangle
+import org.apache.commons.io.FileUtils
+import org.junit.Assert.assertTrue
 import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.util.*
+
 
 class TestLinearRepresentation {
 
     @Test
-    fun mockStackLinearRepresntation() {
+    fun mockStackLinearRepresentation() {
 
         val codeBlock = listOf("let y = new Stack;", "y.push(2);", "y.push(3);", "y.pop();");
 
@@ -28,10 +35,64 @@ class TestLinearRepresentation {
         )
 
         val writer = ManimProjectWriter(ManimWriter(stackIR).build())
-//        writer.generateAnimation(writer.createPythonFile())
+        val generated = writer.createPythonFile()
+        val actual = "src/test/testFiles/python/stack.py"
 
-//        println(ManimWriter(stackIR).build())
-//        assertEquals(ManimDSLParser(inputFile.inputStream()).parseFile().first, ExitStatus.EXIT_SUCCESS)
+        val generatedIsSameAsActual = FileUtils.contentEquals(File(writer.createPythonFile()), File(actual))
+
+        if (!generatedIsSameAsActual) {
+            getDifferenceBetweenGeneratedAndActualFiles(generated, actual)
+        }
+
+        assertTrue("The files generated does not match what is expected", generatedIsSameAsActual)
+    }
+
+    private fun getDifferenceBetweenGeneratedAndActualFiles(generated: String, actual: String) {
+        var sCurrentLine: String?
+        val generatedList: MutableList<String> = ArrayList()
+        val actualList: MutableList<String> = ArrayList()
+
+        /* Read files into list of strings */
+        val br1 = BufferedReader(FileReader(generated))
+        val br2 = BufferedReader(FileReader(actual))
+
+        while (br1.readLine().also { sCurrentLine = it } != null) {
+            generatedList.add(sCurrentLine!!)
+        }
+        while (br2.readLine().also { sCurrentLine = it } != null) {
+            actualList.add(sCurrentLine!!)
+        }
+
+        /* Generate file with differences */
+        val shorter: Pair<List<String>, String>;
+        val longer: Pair<List<String>, String>;
+        if (actualList.size < generatedList.size) {
+            shorter = Pair(actualList, "actual")
+            longer = Pair(generatedList, "generated")
+        } else {
+            shorter = Pair(generatedList, "generated")
+            longer = Pair(actualList, "actual")
+        }
+
+        for (i in longer.first.indices) {
+            /* If i is in range of shorter file */
+            if (i in shorter.first.indices) {
+                /* If line at i is not equal in both files print difference otherwise print line */
+                if (shorter.first[i] != longer.first[i]) {
+                    println(">>>>>>>>>>>>> ${shorter.second} >>>>>>>>>>>>>>")
+                    println("$i: ${shorter.first[i]}")
+                    println("<<<<<<<<<<<<<<< ${longer.second} <<<<<<<<<<<<<<<")
+                    println("$i: ${longer.first[i]}")
+                } else {
+                    println("$i: ${longer.first[i]}")
+                }
+            } else {
+                /* Print missing lines that are at end of the file */
+                println(">>>>>>>>>>>>> ${shorter.second} >>>>>>>>>>>>>>")
+                println("<<<<<<<<<<<<<<< ${longer.second}  <<<<<<<<<<<<<<<")
+                println("$i: ${longer.first[i]}")
+            }
+        }
     }
 
 }
---
+++ src/test/testFiles/python/stack.py
@@ -0,0 +1,58 @@
+from manimlib.imports import *
+ 
+class Main(Scene):
+    def construct(self):
+        code_block = Code_block(["let y = new Stack;","y.push(2);","y.push(3);","y.pop();"])
+        code_text = code_block.build()
+        self.place_at(code_text, -1, 0)
+        self.play(FadeIn(code_text))
+        pointer = ArrowTip(color=YELLOW).scale(0.7).flip(TOP)
+        self.move_arrow_to_line(1, pointer, code_block)
+        y = Line()
+        y.to_edge(np.array([2,-1, 0]))
+        self.play(ShowCreation(y))
+        testIdent = Rectangle_block("2").build()
+        self.move_arrow_to_line(2, pointer, code_block)
+        self.move_relative_to_obj(testIdent, y, 0.0, 0.25)
+        self.move_arrow_to_line(3, pointer, code_block)
+        testIdent1 = Rectangle_block("3").build()
+        self.move_relative_to_obj(testIdent1, testIdent, 0.0, 0.25)
+        self.wait(2.0)
+        self.move_arrow_to_line(4, pointer, code_block)
+        self.move_relative_to_obj(testIdent1, testIdent, 0.0, 20.25)
+        self.wait(2.0)
+    def place_at(self, group, x, y):
+        group.to_edge(np.array([x, y, 0]))
+    
+    def move_relative_to_edge(self, group, x, y):
+        self.play(ApplyMethod(group.to_edge, np.array([x, y, 0])))
+    
+    def move_relative_to_obj(self, group, target, x, y):
+        self.play(ApplyMethod(group.next_to, target, np.array([x, y, 0])))
+    
+    def move_arrow_to_line(self, line_number, pointer, code_block):
+        line_object = code_block.get_line_at(line_number)
+        self.play(FadeIn(pointer.next_to(line_object, LEFT, MED_SMALL_BUFF)))
+    
+class Code_block():
+    def __init__(self, code):
+        self.code = TextMobject(*code)
+
+    def build(self):
+        return self.code.arrange(DOWN, aligned_edge=LEFT)
+
+    def get_line_at(self, line_number):
+        return self.code[line_number - 1]
+
+class Rectangle_block():
+    def __init__(self, text, height=0.75, width=1.5, color=BLUE):
+        self.text   = text
+        self.height = height
+        self.width  = width
+        self.color  = color
+
+    def build(self):
+        inside_text = TextMobject(self.text)
+        rectangle   = Rectangle(height=self.height, width=self.width, color=self.color)
+        group       = VGroup(inside_text, rectangle)
+        return group
