3ec5964b0f6265ec1fac972fea93715b07914adf
Mayank Sharma <mayank.sharma.uk@gmail.com>
2020-10-17 17:23:42 +0100

    Fixed compile script and created directories if needed

--- src/main/kotlin/com/manimdsl/ManimProjectWriter.kt
+++ src/main/kotlin/com/manimdsl/ManimProjectWriter.kt
@@ -1,12 +1,15 @@
 package com.manimdsl
 
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
 
 class ManimProjectWriter(private val pythonCode: String) {
 
     fun createPythonFile(fileName: String? = null): String {
         return if (fileName !== null) {
+            Files.createDirectories(Paths.get(fileName.split("/").dropLast(1).joinToString("")))
             File(fileName).writeText(pythonCode)
             fileName
         } else {
@@ -19,7 +22,7 @@ class ManimProjectWriter(private val pythonCode: String) {
     fun generateAnimation(fileName: String, options: List<String>, outputFile: String): Int {
         val uid = UUID.randomUUID().toString()
         val commandOptions = options.joinToString(" ")
-        val manimExitCode = ProcessBuilder("manim $fileName Main $commandOptions --video_output_dir $uid --media_dir $uid".split(" "))
+        val manimExitCode = ProcessBuilder("manim $fileName Main $commandOptions --media_dir $uid --video_output_dir $uid".split(" "))
             .start().waitFor()
         val copyExitCode = ProcessBuilder("cp -f $uid/Main.mp4 $outputFile".split(" "))
             .start().waitFor()
