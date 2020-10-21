b7fbee5ede41cc119d290175b4013a47af93bc1f
Arjun Banerjee <43380097+arj119@users.noreply.github.com>
2020-10-21 16:28:47 +0100

    Extend IR to take customisable inputs for aesthetics (#25)
    * Added color/text_color attributes to python library.
    
    * Added text weight and font attributes to python library. Replaced TextMObject with better abstraction Text.
    
    * Implemented StyleAttribute classes to be used in Linear IR
    
    * Added StyleTests.kt and extended NewMObject class to include style parameters.
    
    * Altered InitStructure to take Position type argument to reduce duplication.
    
    * Added getConstructor function in shapes for convenience
    
    * Reused exisitng Coord class for position interface
    
    * Implemented Style class for easier attribute handling. Created CodeBlockShape to reduce duplication
    
    * Implemented InitStructureShape class to reduce duplication
    
    * Altered python library to keep references and expose constituent manim objects for customisation. Altered IR to handle this.
    
    * Changed Shape init block to use it

---
+++ src/test/kotlin/com/manimdsl/linearrepresentation/StyleTests.kt
@@ -0,0 +1,52 @@
+package com.manimdsl.linearrepresentation
+
+import com.manimdsl.shapes.Rectangle
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
+
+
+class StyleTests {
+
+    @Test
+    fun stylesAreMappedToPythonCorrectly() {
+
+        val color = "white"
+        val textColor = "black"
+        val textWeight = "normal"
+        val font = "TestFont"
+        val rectangle = Rectangle("x", "rectangle", color, textColor, textWeight, font)
+
+        val mobject = NewMObject(rectangle, "codeBlock")
+
+        val expected =
+            "x = Rectangle_block(\"rectangle\", color=${color.toUpperCase()}, text_color=${textColor.toUpperCase()}, text_weight=${textWeight.toUpperCase()}, font=\"$font\")"
+        assertEquals(expected, mobject.toPython()[0])
+    }
+
+    @Test
+    fun hexadecimalColorsAreMappedCorrectly() {
+
+        val color = "#ffffff"
+        val textColor = "#ffffff"
+        val rectangle = Rectangle("x", "rectangle", color, textColor)
+
+        val mobject = NewMObject(rectangle, "codeBlock")
+
+        val expected =
+            "x = Rectangle_block(\"rectangle\", color=\"$color\", text_color=\"$textColor\")"
+        assertEquals(expected, mobject.toPython()[0])
+    }
+
+    @Test
+    fun invalidTextWeightsAreReturnedToDefault() {
+
+        val textWeight = "invalid"
+        val rectangle = Rectangle("x", "rectangle", textWeight = textWeight)
+
+        val mobject = NewMObject(rectangle, "codeBlock")
+
+        val expected =
+            "x = Rectangle_block(\"rectangle\", text_weight=NORMAL)"
+        assertEquals(expected, mobject.toPython()[0])
+    }
+}
