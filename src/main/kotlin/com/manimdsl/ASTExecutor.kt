c0370dffc0ae9f2cf885c86a437b5c53ffd48a1a
Aditya Goel <agoel4512@gmail.com>
2020-10-22 15:35:19 +0100

    Function execution correct

--- src/main/kotlin/com/manimdsl/ASTExecutor.kt
+++ src/main/kotlin/com/manimdsl/ASTExecutor.kt
@@ -18,60 +18,92 @@ class VirtualMachine(private val program: ProgramNode, private val symbolTableVi
 
     private val linearRepresentation = mutableListOf<ManimInstr>()
     private val variableNameGenerator = VariableNameGenerator(symbolTableVisitor)
-
     private val codeBlockVariable: String
     private val codeTextVariable: String
     private val pointerVariable: String
-
+    private val realLine: MutableList<Int> = mutableListOf()
+    private val displayCode: MutableList<String> = mutableListOf()
     init {
         pointerVariable = variableNameGenerator.generateNameFromPrefix("pointer")
         codeBlockVariable = variableNameGenerator.generateNameFromPrefix("code_block")
         codeTextVariable = variableNameGenerator.generateNameFromPrefix("code_text")
+        fileLines.indices.forEach {
+            if (it == 0) {
+                if (statements.containsKey(it+1) && statements[it+1] is CodeNode) {
+                    displayCode.add(fileLines[it])
+                    realLine.add(1)
+                } else {
+                    realLine.add(0)
+                }
+            } else {
+                realLine.add(realLine.last() + if (fileLines[it] == "}" || fileLines[it] == "{"  || (statements.containsKey(it+1) && statements[it+1] is CodeNode)) {
+                    displayCode.add(fileLines[it])
+                    1
+                } else {
+                    0
+                })
+            }
+        }F
     }
 
     fun runProgram(): List<ManimInstr> {
         val variables = mutableMapOf<String, ExecValue>()
         val startLine = program.statements.first().lineNumber
-        Frame(startLine, variables).execute()
-        val codeLines = (fileLines.indices).filter { statements.containsKey(it+1) && statements[it+1] !is AnimationNode }.map { fileLines[it] }
-        linearRepresentation.add(0, CodeBlock(codeLines, codeBlockVariable, codeTextVariable, pointerVariable))
+        Frame(startLine, fileLines.size, variables).execute()
+        linearRepresentation.add(0, CodeBlock(displayCode, codeBlockVariable, codeTextVariable, pointerVariable))
         return linearRepresentation
     }
 
-
-    private inner class Frame(private var pc: Int, private var variables: MutableMap<String, ExecValue>) {
+    private inner class Frame(private var pc: Int, private var finalLine: Int, private var variables: MutableMap<String, ExecValue>) {
 
         // will be used for scoping changes e.g. recursion
-        private val child: Frame? = null
 
-        fun execute() {
+        fun execute(): ExecValue {
 
-            while (pc <= fileLines.size) {
+            while (pc <= finalLine) {
 
                 if (statements.containsKey(pc)) {
                     val statement = statements[pc]
 
                     if (statement is CodeNode) {
-                        var numberOfNonCodeLines = 0
-                        for (i in 1 until pc) {
-                            if (!statements.containsKey(i) || statements[i] !is CodeNode) {
-                                numberOfNonCodeLines++
-                            }
-                        }
-                        linearRepresentation.add(MoveToLine(pc - numberOfNonCodeLines, pointerVariable, codeBlockVariable))
+                        moveToLine()
                     }
 
                     when (statement) {
+                        is ReturnNode -> {
+                            return executeExpression(statement.expression)
+                        }
                         is FunctionNode -> {
+                            // just go onto next line, this is just a label
                         }
                         is AssignmentNode -> executeAssignment(statement)
                         is DeclarationNode -> executeAssignment(statement)
                         is MethodCallNode -> executeMethodCall(statement, false)
+                        is FunctionCallNode -> executeFunctionCall(statement)
                     }
                 }
                 fetchNextStatement()
             }
+
+            return EmptyValue
         }
+
+        private fun moveToLine() {
+            linearRepresentation.add(MoveToLine(realLine[pc-1], pointerVariable, codeBlockVariable))
+        }
+
+        private fun executeFunctionCall(statement: FunctionCallNode): ExecValue {
+            // create new stack frame with argument variables
+            val executedArguments = statement.arguments.map { executeExpression(it) }
+            val parameterNodes = (symbolTableVisitor.getData(statement.functionIdentifier) as FunctionData).parameters
+            val argumentVariables = (parameterNodes.map { it.identifier } zip executedArguments).toMap().toMutableMap()
+            val functionNode = program.functions.find { it.identifier == statement.functionIdentifier }!!
+            val finalStatementLine = functionNode.statements.last().lineNumber
+            val functionFrame = Frame(functionNode.lineNumber, finalStatementLine, argumentVariables)
+            // program counter will forward in loop, we have popped out of stack
+            return functionFrame.execute()
+        }
+
 
         private fun fetchNextStatement() {
             ++pc
@@ -83,6 +115,11 @@ class VirtualMachine(private val program: ProgramNode, private val symbolTableVi
                 else -> executeExpression(node.expression)
             }
             variables[node.identifier] = rhsValue
+
+            // to visualise popping back to assignment we can move line to the prior assignment statement again
+            if (node.expression is FunctionCallNode) {
+                moveToLine()
+            }
         }
 
 
@@ -98,7 +135,8 @@ class VirtualMachine(private val program: ProgramNode, private val symbolTableVi
                 is MultiplyExpression -> executeBinaryOp(node) { x, y -> x * y }
                 is PlusExpression -> executeUnaryOp(node) { x -> x }
                 is MinusExpression -> executeUnaryOp(node) { x -> -x }
-                else -> EmptyValue
+                is ConstructorNode -> TODO()
+                is FunctionCallNode -> executeFunctionCall(node)
             }
         }
 
