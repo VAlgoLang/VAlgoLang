14b64c48156c2fee41558fe1b3ded4a0a0020031
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-10-26 15:36:24 +0000

    Added sleeps between if statement branch condition evaluation

--- src/main/kotlin/com/manimdsl/ASTExecutor.kt
+++ src/main/kotlin/com/manimdsl/ASTExecutor.kt
@@ -90,7 +90,11 @@ class VirtualMachine(
         }
 
         private fun executeSleep(statement: SleepNode) {
-            linearRepresentation.add(Sleep((executeExpression(statement.sleepTime) as DoubleValue).value))
+            addSleep((executeExpression(statement.sleepTime) as DoubleValue).value)
+        }
+
+        private fun addSleep(length: Double) {
+            linearRepresentation.add(Sleep(length))
         }
 
         private fun moveToLine(line: Int = pc, updatePc: Boolean = false) {
@@ -270,6 +274,7 @@ class VirtualMachine(
         }
 
         private fun executeIfStatement(ifStatementNode: IfStatementNode): ExecValue {
+            addSleep(1.0)
             var conditionValue = executeExpression(ifStatementNode.condition) as BoolValue
             //If
             if (conditionValue.value) {
@@ -279,6 +284,7 @@ class VirtualMachine(
             // Elif
             for (elif in ifStatementNode.elifs) {
                 moveToLine(elif.lineNumber)
+                addSleep(1.0)
                 // Add statement to code
                 conditionValue = executeExpression(elif.condition) as BoolValue
                 if (conditionValue.value) {
@@ -288,6 +294,7 @@ class VirtualMachine(
 
             // Else
             moveToLine(ifStatementNode.elseBlock.lineNumber)
+            addSleep(1.0)
             return executeStatementBlock(ifStatementNode.elseBlock)
 
         }
