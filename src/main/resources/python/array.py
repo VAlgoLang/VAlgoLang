0f4a9887b6e26696078bd855bf1182b1af3b6899
Rini Banerjee <rbanerjee20@gmail.com>
2020-10-29 13:42:07 +0000

    Fixed animation for assigning values to array elements

--- src/main/resources/python/array.py
+++ src/main/resources/python/array.py
@@ -1,22 +1,20 @@
 class ArrayElem:
-    def __init__(self, val, scale, init_color, width):
+    def __init__(self, val, scale, color, text_color, width):
         self.val = val
-        self.text = Text(str(self.val))
-        self.text.scale(scale)
-        self.group = Rectangle_block(str(self.val), width=width, height=width).all
-        self.group.set_color(init_color)
+        self.rectangle = Rectangle_block(str(self.val), width=width, height=width, color=color, text_color=text_color)
+        self.group = self.rectangle.all
 
 
 class Array:
-    def __init__(self, values, title, boundaries, init_color=BLUE):
+    def __init__(self, values, title, boundaries, color=BLUE, text_color=WHITE):
         self.values = values
         max = 1.5
         size_of_square = (boundaries[1][0] - boundaries[0][0] - 1) / len(values)
         square_width = min(max, size_of_square)
-        self.array_elements = [ArrayElem(val, 1, init_color, square_width) for val in self.values]
+        self.array_elements = [ArrayElem(val, 1, color, text_color, square_width) for val in self.values]
         self.title = VGroup(Text(title).set_width(0.7))
         self.title.move_to(np.array([boundaries[0][0] + 0.35, (boundaries[0][1] + boundaries[3][1]) / 2, 0]))
-        self.init_color = init_color
+        self.color = color
 
     def build(self):
         previous = self.title
--- src/main/resources/python/rectangle.py
+++ src/main/resources/python/rectangle.py
@@ -4,6 +4,14 @@ class Rectangle_block:
         self.shape = Rectangle(height=height, width=width, color=color)
         self.all = VGroup(self.text, self.shape)
         self.text.set_width(7/10 * width)
+        self.width = width
+        self.text_color = text_color
+        self.font = font
         if target:
             self.owner = target
             self.all.scale(max(target.empty.submobjects[1].get_height() / self.shape.get_height(), target.empty.get_width() / self.shape.get_width()))
+
+    def replace_text(self, new_text):
+        new_text_obj = Text(new_text, color=self.text_color, font=self.font)
+        new_text_obj.set_width(self.width * 7/10)
+        return (Transform(self.text, new_text_obj.move_to(self.all.get_center())))
