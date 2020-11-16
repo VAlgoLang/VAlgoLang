ba01533d822c52530ccab4e0cb37556c9f53ae1e
Clara Lebbos <claralebbos@hotmail.com>
2020-11-16 21:23:08 +0000

    Added line wrapping

--- src/main/kotlin/com/manimdsl/runtime/ASTExecutor.kt
+++ src/main/kotlin/com/manimdsl/runtime/ASTExecutor.kt
@@ -47,13 +47,43 @@ class VirtualMachine(
         }
     }
 
+    fun wrapCode(code: MutableList<String>): MutableList<MutableList<String>> {
+        val wrappedCode = mutableListOf<MutableList<String>>()
+        for (line in code) {
+            val wrappedLine = mutableListOf<String>()
+            var index = 0
+            var prevIndex = 0
+            while (index < line.length) {
+                index = wrapLine(line.substring(index, line.length)) + prevIndex
+                wrappedLine.add(line.substring(prevIndex, index))
+                prevIndex = index
+            }
+            wrappedCode.add(wrappedLine)
+        }
+        return wrappedCode
+    }
+
+    fun wrapLine(line : String): Int{
+        val list = line.split(" ")
+        var counter = 0
+        var prevCounter = 0
+        for (word in list) {
+            counter += word.length + 1
+            if (counter >= WRAP_LINE_LENGTH) {
+                return prevCounter
+            }
+            prevCounter = counter
+        }
+        return line.length
+    }
+
     fun runProgram(): Pair<ExitStatus, List<ManimInstr>> {
         if (!hideCode) {
             linearRepresentation.add(PartitionBlock("1/3", "2/3"))
             linearRepresentation.add(VariableBlock(listOf(), "variable_block", "variable_vg", "variable_frame"))
             linearRepresentation.add(
                 CodeBlock(
-                    displayCode.map { it.chunked(WRAP_LINE_LENGTH) },
+                    wrapCode(displayCode),
                     codeBlockVariable,
                     codeTextVariable,
                     pointerVariable
