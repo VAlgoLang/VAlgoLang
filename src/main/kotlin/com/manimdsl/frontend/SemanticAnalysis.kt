9b352da7c3d09b0d682bd6d16b94c0595ed45999
Rini Banerjee <rbanerjee20@gmail.com>
2021-01-03 22:35:09 +0000

    Commented SemanticErrorHandling.kt

--- src/main/kotlin/com/manimdsl/frontend/SemanticAnalysis.kt
+++ src/main/kotlin/com/manimdsl/frontend/SemanticAnalysis.kt
@@ -119,7 +119,7 @@ class SemanticAnalysis {
 
     fun incompatibleTypesCheck(lhsType: Type, rhsType: Type, text: String, ctx: ParserRuleContext) {
         if (rhsType is NullType && lhsType !is NullableDataStructure && lhsType !is NullType) {
-            nonNullableAssignedToNull(rhsType.toString(), lhsType.toString(), ctx)
+            nonNullableAssignedToNull(lhsType.toString(), ctx)
         } else if (rhsType != NullType && lhsType != ErrorType && rhsType != ErrorType && lhsType != rhsType) {
             declareAssignError(text, rhsType, lhsType, ctx)
         }
@@ -564,7 +564,7 @@ class SemanticAnalysis {
 
     fun unableToInferTypeCheck(rhsType: Type, ctx: ParserRuleContext) {
         if (rhsType is NullType) {
-            unableToInferType(rhsType.toString(), ctx)
+            unableToInferTypeFromNullType(ctx)
         }
     }
 
@@ -592,7 +592,7 @@ class SemanticAnalysis {
             if (is2D) "2D " else "1D "
         } else ""
         if ((initialiser is Array2DInitialiserNode && !is2D) || (initialiser is DataStructureInitialiserNode && is2D)) {
-            incompatibleInitialisation("${arrayPrefix}$dataStructureType", ctx)
+            incompatibleDataStructureInitialisation("${arrayPrefix}$dataStructureType", ctx)
         }
     }
 
