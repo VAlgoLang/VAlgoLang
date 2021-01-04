631b843a3804a1a5884e5e0689c9fb0b47fe01d2
ilonazhu <ilonazhu.825@gmail.com>
2021-01-04 19:04:56 +0000

    Commented ManimProjectWriter.kt

--- src/main/kotlin/com/manimdsl/animation/ManimProjectWriter.kt
+++ src/main/kotlin/com/manimdsl/animation/ManimProjectWriter.kt
@@ -5,8 +5,21 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.*
 
+/**
+ * Writer that produces the output animation video and/or python file
+ *
+ * @property pythonCode: string containing all the python code that generates the animation
+ * @constructor Create empty Manim project writer
+ */
 class ManimProjectWriter(private val pythonCode: String) {
 
+    /**
+     * Create and write the python code generated to a python file if input fileName is provided,
+     * otherwise create and write to a temporary file
+     *
+     * @param fileName
+     * @return output file name or the path to the temporary file
+     */
     fun createPythonFile(fileName: String? = null): String {
         return if (fileName !== null) {
             Files.createDirectories(Paths.get(fileName.split("/").dropLast(1).joinToString("")))
@@ -19,6 +32,14 @@ class ManimProjectWriter(private val pythonCode: String) {
         }
     }
 
+    /**
+     * Generate animation using the python file (temporary or disk) created before
+     *
+     * @param fileName: name of python file to be executed
+     * @param options: CLI options for generating manim animation
+     * @param outputFile: name of output mp4 file
+     * @return exit code from generating animation and writing it to output mp4
+     */
     fun generateAnimation(fileName: String, options: List<String>, outputFile: String): Int {
         Files.createDirectories(Paths.get(outputFile.split("/").dropLast(1).joinToString("")))
         val uid = UUID.randomUUID().toString()
